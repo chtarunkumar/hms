661832e1c5344c16b3ff52c2df1a0004d0bb83a6
Tarun kumar <chenchali.cse.20063@iiitbh.ac.in>
2025-11-07 15:32:25 +0530

    Changes

--- app/config.py
+++ app/config.py
@@ -3,4 +3,4 @@ from_address = "tarunkumarchanchali@gmail.com"
 # Generate in Google Account -> Security -> App passwords (Two-Step Verification)
 app_password = "qbwjkhuboadpsjvu"  
 # Receiver address
-to_address = "hk0362691@gmail.com"
+to_address = "gmaheswaranmca@gmail.com"
